305758bca1a5dfd476ed7fcc8bc7c83df6fe94b4
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2019-09-05 17:37:36 -0500

    Inicio del cambio de librería CSS => PLAZreset-CSS v4.0.3

--- includes/_header.php
+++ includes/_header.php
@@ -6,14 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD PHP-MySQL</title>
     <!-- Boostrap 4 - CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
     <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/c762757b32.js"></script>
+    <!-- PLAZreset-CSS 4.0.3 -->
+    <link rel="stylesheet" href="./css/plaz-reset.css">
+    <!-- My CSS main -->
+    <link rel="stylesheet" href="./css/main.css">
 </head>
 <body>
 
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="container">
-            <a href="index.php" class="navbar-brand">CRUD Php-MySQL</a>
+    <nav class="navbar bg-sec">
+        <div class="container-100">
+            <a href="index.php" title="Plazmedia home">
+                <img src="./img/logotype-white1.svg" alt="Logotipo" class="logo-img">
+            </a>
+            <nav>
+                <ul class="navbar-nav">
+                    <small>
+                        Repositorio:
+                    </small>
+                    <li><a href="" class=" white">
+                        <i class="fab fa-github"></i>
+                    </a></li>
+                    <li><a href="" class=" white">
+                        <i class="fab fa-gitlab"></i>
+                    </a></li>
+                    <li><a href="" class=" white">
+                        <i class="fab fa-bitbucket"></i>
+                    </a></li>
+                    <small>v4.0.3</small>
+                    <li><a href="" class="txt-tag-r tag-s tag-plaz">
+                        PLAZreset-CSS
+                        <i class="fas fa-cloud-download-alt"></i>
+                    </a></li>
+                </ul>
+            </nav>
         </div>
     </nav>
